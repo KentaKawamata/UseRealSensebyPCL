24ba77b01c8364806941fd8e80df03edc4f65561
KentaKawamata <s16c1044sc@s.chibakoudai.jp>
2018-10-21 21:29:35 +0900

    success

--- mergeCloud/mergeCloud.cpp
+++ mergeCloud/mergeCloud.cpp
@@ -2,3 +2,32 @@
 // Created by kawa on 10/21/18.
 //
 
+#include <iostream>
+#include <pcl/io/ply_io.h>
+#include <pcl/io/io.h>
+#include <pcl/point_types.h>
+
+pcl::PointCloud<pcl::PointXYZRGB> merge_cloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud1, pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud2) {
+
+    //２つの点群をまとめて格納する
+    pcl::PointCloud<pcl::PointXYZRGB> cloud_merged;
+
+    cloud_merged = *cloud1;
+    cloud_merged += *cloud2;
+
+    return cloud_merged;
+}
+
+int main(int argc, char *argv[]) {
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud1 (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud2 (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+
+    pcl::io::loadPLYFile(argv[1], *cloud1);
+    pcl::io::loadPLYFile(argv[2], *cloud2);
+
+    pcl::io::savePLYFileBinary("merged.ply" , merge_cloud(cloud1, cloud2));
+
+    return 0;
+}
